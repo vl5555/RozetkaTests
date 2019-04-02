6034cb50b9f94df2a283d8b947d158f34f05558f
vl5555 <v.a.pavlychenko@gmail.com>
2019-04-02 21:01:43 +0300

    first test

--- RozetkaTests/UnitTest1.cs
+++ RozetkaTests/UnitTest1.cs
@@ -1,18 +1,38 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 
 namespace Tests
 {
     public class Tests
     {
+        IWebDriver driver;
+
         [SetUp]
-        public void Setup()
+        public void SetUp()
         {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();            
+            driver.Navigate().GoToUrl("https://rozetka.com.ua/");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+           // driver.Quit();
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            IWebElement firstElement = driver.FindElement(By.XPath("//ul[@class='menu-categories menu-categories_type_main']/li[1]/a"));
+            IWebElement secondElement = driver.FindElement(By.XPath("///a[@href='https://rozetka.com.ua/notebooks/asus/c80004/v004/']"));
+
+            Actions actions = new Actions(driver);
+            actions.Release(firstElement).MoveToElement(secondElement).Click().Perform();
+            //Assert.Pass();
         }
     }
 }
